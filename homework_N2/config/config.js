3d630c30b3d1788869587dc43094e0f4f43f205a
giorgiakhvlediani <86974239+giorgiakhvlediani@users.noreply.github.com>
2022-05-15 23:39:12 +0400

    Add files via upload

---
+++ homework_N2/config/config.js
@@ -0,0 +1,14 @@
+export const config = {
+    searched: {
+        url: "https://api.giphy.com/v1/gifs/search?",
+        limit: 10,
+        api_key: "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB",
+        fmt: "json",
+    },
+    trending: {
+        url: "https://api.giphy.com/v1/gifs/trending?",
+        limit: 10,
+        api_key: "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB",
+        fmt: "json",
+    }
+   };
---
+++ homework_N2/scripts.js
@@ -0,0 +1,59 @@
+import { searchButtonSection } from "./common/searchButtonSection.js";
+import { buttonsData } from "./buttons/buttonsData.js";
+import { gifsSection } from "./common/gifsSection.js"
+import { config } from "./config/config.js"
+
+let submitButton = document.getElementById("submit");
+let buttonsList = document.getElementsByClassName("button");
+console.log(buttonsList, "ka");
+clickingButton(buttonsList);
+
+submitButton.addEventListener('click', (event) => {
+  let searchValue = document.getElementById("user-search").value;
+  if (searchValue !== "") {
+    buttonsData.push(searchValue);
+    let id = document.getElementById('buttons');
+    let searchBlock = new searchButtonSection(id);
+    searchBlock.setButtonList = buttonsData;
+    searchBlock.render()
+    buttonsList = document.getElementsByClassName("button");
+    let searchedGif = searchValue;
+    searchingUrl(searchedGif);
+  }
+  clickingButton(buttonsList);
+})
+
+let trendingButton = document.getElementById("trending");
+
+trendingButton.addEventListener('click', (event) => {
+  let url = `${config.trending.url}&limit=${config.trending.limit}&api_key=${config.trending.api_key}&fmt=${config.searched.fmt}`
+  json(url);
+})
+
+function clickingButton() {
+  for (let i = 0; i < buttonsList.length; i++) {
+    buttonsList[i].addEventListener('click', (event) => {
+      let searchedGif = buttonsList[i].value;
+      searchingUrl(searchedGif);
+    })
+  }
+}
+
+function searchingUrl(searchedGif) {
+  let url = `${config.searched.url}q=${searchedGif}&limit=${config.searched.limit}&api_key=${config.searched.api_key}&fmt=${config.searched.fmt}`
+  json(url);
+}
+
+let id = document.getElementById('gif-boxes');
+let gifsBlock = new gifsSection(id);
+
+function json(url) {
+  let promise = fetch(url);
+  promise.then((res) => {
+    return res.json();
+  }).then((res) => {
+    gifsBlock.setGifList = res.data;
+    gifsBlock.render();
+  })
+}
+
